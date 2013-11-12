a1276c1522244d72434680935f2ab211b7ac5763
AndrewJSeverson <severa24@uwosh.edu>
2013-11-12 16:57:40 -0600

    Showing menu form

---
+++ TileSetGenerator/Menu.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace TileSetGenerator
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnTuring = new System.Windows.Forms.Button();
+            this.btnSquare = new System.Windows.Forms.Button();
+            this.btnRectangle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(348, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please choose a tile set you would like to create:";
+            // 
+            // btnTuring
+            // 
+            this.btnTuring.Location = new System.Drawing.Point(12, 50);
+            this.btnTuring.Name = "btnTuring";
+            this.btnTuring.Size = new System.Drawing.Size(95, 23);
+            this.btnTuring.TabIndex = 1;
+            this.btnTuring.Text = "Turing Machine";
+            this.btnTuring.UseVisualStyleBackColor = true;
+            this.btnTuring.Click += new System.EventHandler(this.btnTuring_Click);
+            // 
+            // btnSquare
+            // 
+            this.btnSquare.Location = new System.Drawing.Point(158, 50);
+            this.btnSquare.Name = "btnSquare";
+            this.btnSquare.Size = new System.Drawing.Size(75, 23);
+            this.btnSquare.TabIndex = 2;
+            this.btnSquare.Text = "Square";
+            this.btnSquare.UseVisualStyleBackColor = true;
+            this.btnSquare.Click += new System.EventHandler(this.btnSquare_Click);
+            // 
+            // btnRectangle
+            // 
+            this.btnRectangle.Location = new System.Drawing.Point(285, 50);
+            this.btnRectangle.Name = "btnRectangle";
+            this.btnRectangle.Size = new System.Drawing.Size(75, 23);
+            this.btnRectangle.TabIndex = 3;
+            this.btnRectangle.Text = "Rectangle";
+            this.btnRectangle.UseVisualStyleBackColor = true;
+            this.btnRectangle.Click += new System.EventHandler(this.btnRectangle_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 93);
+            this.Controls.Add(this.btnRectangle);
+            this.Controls.Add(this.btnSquare);
+            this.Controls.Add(this.btnTuring);
+            this.Controls.Add(this.label1);
+            this.Name = "Menu";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnTuring;
+        private System.Windows.Forms.Button btnSquare;
+        private System.Windows.Forms.Button btnRectangle;
+    }
+}
+
---
+++ TileSetGenerator/Menu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TileSetGenerator
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTuring_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var turing = new TuringSetup1();
+            turing.Show();
+        }
+
+        private void btnSquare_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var square = new SquareSetup1();
+            square.Show();
+        }
+
+        private void btnRectangle_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var rectangle = new RectangleSetup1();
+            rectangle.Show();
+        }
+    }
+}
